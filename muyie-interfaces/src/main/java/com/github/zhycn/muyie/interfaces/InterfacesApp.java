d417092732c051d98d51827f082d7ed554aa488a
qizhaohong <qizhaohong_sh@qq.com>
2020-04-27 11:35:24 +0800

    Initial commit

--- muyie-interfaces/src/main/java/com/github/zhycn/muyie/interfaces/InterfacesApp.java
+++ muyie-interfaces/src/main/java/com/github/zhycn/muyie/interfaces/InterfacesApp.java
@@ -1,14 +1,14 @@
-package com.github.zhycn.muyie.application;
+package com.github.zhycn.muyie.interfaces;
 
 import org.muyie.framework.config.DefaultProfileUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class InterfacesApp {
 
   public static void main(String[] args) throws Exception {
-    SpringApplication application = new SpringApplication(Application.class);
+    SpringApplication application = new SpringApplication(InterfacesApp.class);
     DefaultProfileUtil.addDefaultProfile(application);
     application.run(args);
   }
--- muyie-scheduler/src/main/java/com/github/zhycn/muyie/scheduler/SchedulerApp.java
+++ muyie-scheduler/src/main/java/com/github/zhycn/muyie/scheduler/SchedulerApp.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class SchedulerApp {
 
   public static void main(String[] args) throws Exception {
-    SpringApplication application = new SpringApplication(Application.class);
+    SpringApplication application = new SpringApplication(SchedulerApp.class);
     DefaultProfileUtil.addDefaultProfile(application);
     application.run(args);
   }
